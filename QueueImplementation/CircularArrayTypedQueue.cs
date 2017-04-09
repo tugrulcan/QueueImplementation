319c7106aedd0b3036c81e667764b7fbfbfa7905
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-10 01:14:51 +0300

    Add CircularArrayTypedQueue

---
+++ QueueImplementation/CircularArrayTypedQueue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementation
+{
+    public class CircularArrayTypedQueue : IQueue
+    {
+        int front = -1;
+        int rear = -1;
+        int size = 0;
+        public int count = 0;
+
+        private object[] items_array;
+
+        public CircularArrayTypedQueue(int size)
+        {
+            this.size = size;
+            this.items_array = new object[size];
+        }
+
+        public void Insert(object item)
+        {
+            if (count == 0)
+            {
+                front = rear = 0;
+                this.items_array[rear] = item;
+                this.count++;
+            }
+            else if (count == size)
+            {
+                throw new OverflowException();
+            }
+            else
+            {
+                rear = (rear + 1) % size;
+                this.items_array[rear] = item;
+                this.count++;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return this.count == 0 ? true : false;
+        }
+
+        public object Peek()
+        {
+            return this.items_array[front];
+        }
+
+        public object Remove()
+        {
+            object removed_item = null;
+            if (count == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                this.count--;
+                removed_item = this.items_array[front];
+                front = (front + 1) % size;
+            }
+
+            return removed_item;
+        }
+
+        public string DisplayElements()
+        {
+            string msg = "";
+
+            if (this.count == 0)
+            {
+                msg = "";
+            }
+            else
+            {
+
+                for (int i = front; ; i = (i + 1) % size)
+                {
+                    msg += this.items_array[i].ToString() + " ";
+                    if (i == rear)
+                        break;
+                }
+
+            }
+
+            return msg;
+        }
+    }
+}
---
+++ QueueUnitTestProject/CircularArrayTypedQueueUnitTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueueImplementation;
+
+
+namespace QueueUnitTestProject
+{
+    [TestClass]
+    public class CircularArrayTypedQueueUnitTests
+    {
+        [TestMethod]
+        public void KuyrukBoskenDisplayElementsDuzgunYazdiriyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(10);
+            string beklenen = "";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenDisplayElementsDuzgunYazdiriyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+
+            string beklenen = "1 2 3 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+
+        }
+
+        [TestMethod]
+        public void KuyrukBoskenInsertKuyrugaElemanEkliyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(3);
+            queue.Insert(1);
+
+            string beklenen = "1 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenInsertKuyrugaElemanEkliyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+
+            string beklenen = "1 2 3 ";
+            string asil = queue.DisplayElements();
+
+            Assert.AreEqual(beklenen, asil);
+
+            int beklenen_eleman_sayisi = 3;
+            int asil_eleman_sayisi = queue.count;
+            Assert.AreEqual(beklenen_eleman_sayisi, asil_eleman_sayisi);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void KuyrukDoluykenInsertExceptionFirlatiyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(2);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void KuyrukBoskenremoveExceptionFirlatiyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(2);
+            queue.Remove();
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenRemoveElemaniCikariyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+            object removed_item = queue.Remove();
+            Assert.AreEqual("1", removed_item.ToString());
+            Assert.AreEqual(2, queue.count);
+
+            string beklenen = "2 3 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenOndekiElemanlarSilininceSiraBozuluyorMu()
+        {
+            CircularArrayTypedQueue queue = new CircularArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+            queue.Remove();
+            queue.Remove();
+            queue.Insert(4);
+
+            string beklenen = "3 4 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+            Assert.AreEqual(2, queue.count);
+
+        }
+
+    }
+}
