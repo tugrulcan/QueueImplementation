c26d3d2def36982dd612fe36e4636fc467af8711
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-10 03:07:11 +0300

    Add PriorityArrayTypedQueue

---
+++ QueueImplementation/PriorityArrayTypedQueue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementation
+{
+    public class PriorityArrayTypedQueue : IQueue
+    {
+        int front = -1;
+        int rear = -1;
+        public int count = 0;
+        int size = 0;
+
+        object[] items_array;
+
+
+        public PriorityArrayTypedQueue(int size)
+        {
+            this.size = size;
+            this.items_array = new object[size];
+        }
+
+        public void Insert(object item)
+        {
+            if (count == 0)
+            {
+                front++;
+                rear++;
+                this.count++;
+                this.items_array[front] = item;
+            }
+            else if (count == size)
+            {
+                throw new OverflowException();
+            }
+            else
+            {
+                int i;
+                for (i = front; i >= 0; i--)
+                {
+                    if ((int)item > (int)items_array[i])
+                        items_array[i + 1] = items_array[i];
+                    else
+                        break;
+                }
+                items_array[i + 1] = item;
+                count++;
+                front++;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return this.count == 0 ? true : false;
+        }
+
+        public object Peek()
+        {
+            return items_array[front];
+        }
+
+        public object Remove()
+        {
+            object removed_item = null;
+            if (count == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                removed_item = items_array[front];
+                items_array[front] = null;
+                front--;
+                count--;
+            }
+
+            return removed_item;
+        }
+
+        public string DisplayElements()
+        {
+            string msg = null;
+            if (count == 0)
+                msg = "";
+            else
+            {
+                for (int i = front; i >= 0; i--)
+                {
+                    msg += items_array[i].ToString() + " ";
+                }
+            }
+            return msg;
+        }
+
+
+    }
+}
---
+++ QueueUnitTestProject/PriorityArrayTypedQueueUnitTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueueImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUnitTestProject
+{
+    [TestClass]
+    public class PriorityArrayTypedQueueUnitTests
+    {
+        [TestMethod]
+        public void KuyrukBoskenDisplayElementsDuzgunYazdiriyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(10);
+            string beklenen = "";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenDisplayElementsDuzgunYazdiriyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(5);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(4);
+            queue.Insert(5);
+            queue.Insert(3);
+
+            string beklenen = "1 2 3 4 5 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+        }
+
+        [TestMethod]
+        public void KuyrukBoskenInsertKuyrugaElemanEkliyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(3);
+            queue.Insert(1);
+
+            string beklenen = "1 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenInsertKuyrugaElemanEkliyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+
+            string beklenen = "1 2 3 ";
+            string asil = queue.DisplayElements();
+
+            Assert.AreEqual(beklenen, asil);
+
+            int beklenen_eleman_sayisi = 3;
+            int asil_eleman_sayisi = queue.count;
+            Assert.AreEqual(beklenen_eleman_sayisi, asil_eleman_sayisi);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void KuyrukDoluykenInsertExceptionFirlatiyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(2);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void KuyrukBoskenremoveExceptionFirlatiyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(2);
+            queue.Remove();
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenRemoveElemaniCikariyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+            object removed_item = queue.Remove();
+            Assert.AreEqual("1", removed_item.ToString());
+            Assert.AreEqual(2, queue.count);
+
+            string beklenen = "2 3 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+        }
+
+        [TestMethod]
+        public void KuyruktaElemanVarkenOndekiElemanlarSilininceSiraBozuluyorMu()
+        {
+            PriorityArrayTypedQueue queue = new PriorityArrayTypedQueue(3);
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+            queue.Remove();
+            queue.Remove();
+            queue.Insert(4);
+
+            string beklenen = "3 4 ";
+            string asil = queue.DisplayElements();
+            Assert.AreEqual(beklenen, asil);
+
+            Assert.AreEqual(2, queue.count);
+
+        }
+
+    }
+}
